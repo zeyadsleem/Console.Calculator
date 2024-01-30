79e9c029224a1ba3a1c0f78197f733e8275439f6
zeyadsleem <zeyadsleem1@gmail.com>
2024-01-30 16:12:22 +0200

    apply Sepration of Concerns to app, with oop

---
+++ GameEngin.cs
@@ -0,0 +1,162 @@
+﻿namespace Calculator;
+
+internal class GameEngine
+{
+    internal void DivisionGame(string message)
+    {
+        int score = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+
+            int[] divisionNumbers = Helpers.GetDivisionNumbers();
+            int firstNumber = divisionNumbers[0];
+            int secondNumber = divisionNumbers[1];
+
+            Console.WriteLine($"{firstNumber} / {secondNumber}");
+            string? result = Console.ReadLine();
+
+            if (int.Parse(result) == firstNumber / secondNumber)
+            {
+                Console.WriteLine("Your answer was correct! Type any key for the next question");
+                score++;
+                _ = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Your answer was incorrect. Type any key for the next question");
+                _ = Console.ReadLine();
+            }
+
+            if (i == 4)
+            {
+                Console.WriteLine($"Game over. Your final score is {score}");
+            }
+        }
+
+        Helpers.AddToHistory(score, "Division");
+    }
+
+    internal void MultiplicationGame(string message)
+    {
+        Random random = new();
+        int score = 0;
+
+        int firstNumber;
+        int secondNumber;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+
+            firstNumber = random.Next(1, 9);
+            secondNumber = random.Next(1, 9);
+
+            Console.WriteLine($"{firstNumber} * {secondNumber}");
+            string? result = Console.ReadLine();
+
+            if (int.Parse(result) == firstNumber * secondNumber)
+            {
+                Console.WriteLine("Your answer was correct! Type any key for the next question");
+                score++;
+                _ = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Your answer was incorrect. Type any key for the next question");
+                string? v = Console.ReadLine();
+            }
+
+            if (i == 4)
+            {
+                Console.WriteLine($"Game over. Your final score is {score}");
+            }
+        }
+
+        Helpers.AddToHistory(score, "Multiplication");
+    }
+
+    internal void SubtractionGame(string message)
+    {
+        Random random = new();
+        int score = 0;
+
+        int firstNumber;
+        int secondNumber;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+
+            firstNumber = random.Next(1, 9);
+            secondNumber = random.Next(1, 9);
+
+            Console.WriteLine($"{firstNumber} - {secondNumber}");
+            string? result = Console.ReadLine();
+
+            if (int.Parse(result) == firstNumber - secondNumber)
+            {
+                Console.WriteLine("Your answer was correct! Type any key for the next question");
+                score++;
+                _ = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Your answer was incorrect. Type any key for the next question");
+                _ = Console.ReadLine();
+            }
+
+            if (i == 4)
+            {
+                Console.WriteLine($"Game over. Your final score is {score}");
+            }
+        }
+
+        Helpers.AddToHistory(score, "Subtraction");
+    }
+
+    internal void AdditionGame(string message)
+    {
+        Random random = new();
+        int score = 0;
+
+        int firstNumber;
+        int secondNumber;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+
+            firstNumber = random.Next(1, 9);
+            secondNumber = random.Next(1, 9);
+
+            Console.WriteLine($"{firstNumber} + {secondNumber}");
+            string? result = Console.ReadLine();
+
+            if (int.Parse(result) == firstNumber + secondNumber)
+            {
+                Console.WriteLine("Your answer was correct! Type any key for the next question");
+                score++;
+                _ = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Your answer was incorrect. Type any key for the next question");
+                _ = Console.ReadLine();
+            }
+
+            if (i == 4)
+            {
+                Console.WriteLine($"Game over. Your final score is {score}. Press any key to go back to the main menu.");
+                _ = Console.ReadLine();
+            }
+        }
+
+        Helpers.AddToHistory(score, "Addition");
+    }
+}
---
+++ Helpers.cs
@@ -0,0 +1,45 @@
+﻿namespace Calculator;
+
+internal class Helpers
+{
+    internal static List<string> games = [];
+
+    internal static int[] GetDivisionNumbers()
+    {
+        Random random = new();
+        int firstNumber = random.Next(1, 99);
+        int secondNumber = random.Next(1, 99);
+
+        int[] result = new int[2];
+
+        while (firstNumber % secondNumber != 0)
+        {
+            firstNumber = random.Next(1, 99);
+            secondNumber = random.Next(1, 99);
+        }
+
+        result[0] = firstNumber;
+        result[1] = secondNumber;
+
+        return result;
+    }
+
+    internal static void AddToHistory(int gameScore, string gameType)
+    {
+        games.Add($"{DateTime.Now} - {gameType}: {gameScore} pts");
+    }
+
+    internal static void PrintGames()
+    {
+        Console.Clear();
+        Console.WriteLine("Games History");
+        Console.WriteLine("---------------------------");
+        foreach (string game in games)
+        {
+            Console.WriteLine(game);
+        }
+        Console.WriteLine("---------------------------\n");
+        Console.WriteLine("Press any key to return to Main Menu");
+        _ = Console.ReadLine();
+    }
+}
